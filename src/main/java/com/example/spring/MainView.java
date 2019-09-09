f237a9b166673988071b823b0da66e53a8d4033c
Bernd Kleine-Börger <Bernd.Kleine-Boerger@LMIS.de>
2019-09-09 11:52:28 +0200

    addon css import bug?

--- src/main/java/com/example/spring/MainView.java
+++ src/main/java/com/example/spring/MainView.java
@@ -2,9 +2,11 @@ package com.example.spring;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
+import com.bkb.vaadin.progressline.ProgressLine;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.confirmdialog.ConfirmDialog;
 import com.vaadin.flow.component.confirmdialog.ConfirmDialog.ConfirmEvent;
+import com.vaadin.flow.component.dependency.CssImport;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
@@ -12,15 +14,21 @@ import com.vaadin.flow.server.PWA;
 
 @Route(value = "", layout=MainLayout.class)
 @PWA(name = "Project Base for Vaadin Flow with Spring", shortName = "Project Base")
+@CssImport("./styles/main-view.css")
 public class MainView extends VerticalLayout {
 
     public MainView(@Autowired MessageBean bean) {
+    	this.addClassName("main-view");
         Button button = new Button("Click me",
                 e -> Notification.show(bean.getMessage()));
         add(button);
         
         Button btnConfirmDialog = new Button("ConfirmDialog", e-> showConfirmDialog());
         add(btnConfirmDialog);
+        
+        ProgressLine progressLine = new ProgressLine("test");
+        progressLine.setItems("Prozessschritt 1", "Prozessschritt 2", "Prozessschritt 3", "Prozessschritt 4");
+        add(progressLine);
     }
     
     private void showConfirmDialog() {
